4cca4fd40ddbd268f409cc0eb906683fddd10856
Dan Bohdanenko <danbohdanenko@gmail.com>
2023-09-10 19:14:22 +0200

    modified:   src/components/Success/Success.js 	modified:   src/components/contact/Contact.js

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { AiOutlineCheck } from "react-icons/ai";
 import "./success.scss";
+import { Section, Typography } from "../";
 
 export const Success = () => {
   return (
-    <section className="success">
-      <div className="container">
-        <div className="success__body">
-          <h4 className="success__title">Success!</h4>
-          <div className="success__container">
-            <AiOutlineCheck className="success__icon" />
-          </div>
-          <h5 className="success__text">Thank you for your message!</h5>
+    <Section className="success" variant="section">
+      <div className="success__body">
+        <Typography className="success__title" variant="h4">
+          Success!
+        </Typography>
+        <div className="success__container">
+          <AiOutlineCheck className="success__icon" />
         </div>
+        <Typography className="success__text" variant="h5">
+          Thank you for your message!
+        </Typography>
       </div>
-    </section>
+    </Section>
   );
 };
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./contact.scss";
 import { Typography, Section, Form, contactInfo } from "../";
 
